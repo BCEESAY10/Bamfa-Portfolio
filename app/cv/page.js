c3502db65cdfa428f8b7184cee5c8802247463db
BCEESAY10 <bamfaceesay30@gmail.com>
2025-10-29 16:22:32 +0000

    update cv

--- app/cv/page.js
+++ app/cv/page.js
@@ -10,20 +10,18 @@ export default function CVPage() {
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          className="max-w-6xl mx-auto"
-        >
+          className="max-w-6xl mx-auto">
           {/* Header */}
           <motion.div
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
-            className="text-center mb-16"
-          >
+            className="text-center mb-16">
             <h1 className="text-4xl md:text-5xl font-bold text-gray-800 dark:text-white mb-6">
               My CV
             </h1>
             <div className="w-20 h-1 bg-primary mx-auto mb-8" />
             <p className="text-gray-600 dark:text-gray-300 md:text-xl max-w-2xl mx-auto">
-              Here you can view my up-to-date CV. 
+              Take a look at my latest CV.
             </p>
           </motion.div>
 
@@ -31,10 +29,9 @@ export default function CVPage() {
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            className="w-full max-w-4xl shadow-lg rounded-lg overflow-hidden bg-white dark:bg-gray-900 mx-auto"
-          >
+            className="w-full max-w-4xl shadow-lg rounded-lg overflow-hidden bg-white dark:bg-gray-900 mx-auto">
             <iframe
-              src="/Bamfa_CV-June_2025.pdf"
+              src="/Bamfa_CV-Oct-2025.pdf"
               title="CV PDF"
               className="w-full h-[80vh] min-h-[500px] border-0"
             />
